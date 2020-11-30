3118c1795afadb97ffe762811772bae416c4248b
breiner andres jaimes miranda <breinerandresjaimesmiranda@hotmail.com>
2020-11-30 16:09:01 -0500

    .

--- ACTIVIDAD/Form1.cs
+++ ACTIVIDAD/Form1.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ACTIVIDAD
-{
+{ 
     public partial class Form1 : Form
     {
         private SqlConnection Conexion = null;
